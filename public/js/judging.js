47703fe31333154397bccbe05522a3174bf2a49d
Christina Milner <christina@christina-milner.dev>
2023-07-02 13:43:59 +0100

    Disallow CORR if max of 6 reached

--- public/js/judging.js
+++ public/js/judging.js
@@ -154,6 +154,7 @@ async function checkCORRR(checkbox) {
         if (CORRR.length < 6) {
             document.querySelector('#warning').classList.add('info')
         }
+        else {checkbox.checked = false}
         document.querySelector('#warning').classList.remove('hidden')
     } else {
         document.querySelector('#warning').classList.add('hidden')
